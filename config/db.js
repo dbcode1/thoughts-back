d1aa5591ea54baa207105829a222e929b5894d3e
dmb-tech <65574932+dmb-tech@users.noreply.github.com>
2023-07-05 19:45:13 -0400

    db

--- config/db.js
+++ config/db.js
@@ -1,21 +1,13 @@
 const mongoose = require("mongoose");
 const config = require("config");
 const db = config.get("DATABASE_URI");
-require('dotenv').config()
+require("dotenv").config();
 
-const connectDB = async () => {
-  try {
-    await mongoose.connect(process.env.DATABASE_URI, {
-      useNewUrlParser: true,
-      useUnifiedTopology: true
-    });
-
-    console.log("MongoDB Connected...");
-  } catch (err) {
-    console.error(err.message);
-    // Exit process with failure
-    process.exit(1);
+const connectDB = mongoose.connect(
+  process.env.DATABASE_URI,
+  { useNewUrlParser: true, useUnifiedTopology: true },
+  () => {
+    console.log("Connected to MongoDB");
   }
-};
-
+);
 module.exports = connectDB;

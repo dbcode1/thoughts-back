02d8d206fe62369f8560ce9d69e61b1d26486b3e
dmb-tech <65574932+dmb-tech@users.noreply.github.com>
2023-07-05 22:28:42 -0400

    user.find buffering error

--- server.js
+++ server.js
@@ -45,21 +45,11 @@ if (process.env.NODE_ENV === "production") {
   app.use(express.static("client/build"));
 }
 
-const connectDB = async () => {
-  try {
-    await mongoose.connect(process.env.DATABASE_URI, {
-      useNewUrlParser: true,
-      useUnifiedTopology: true,
-    });
-
-    console.log("MongoDB Connected...");
-  } catch (err) {
-    console.error(err.message);
-    // Exit process with failure
-    process.exit(1);
+mongoose.connect(
+  process.env.DB_CONNECTION,
+  { useNewUrlParser: true, useUnifiedTopology: true },
+  () => {
+    console.log("Connected to DB!");
   }
-};
-
-connectDB()
-    app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
-
+);
+app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
